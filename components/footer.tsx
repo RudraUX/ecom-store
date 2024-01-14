bbb940f0eae480f05c8015d71ecf67baab5797fe
RudraUX <pollabrock@gmail.com>
2024-01-14 18:54:12 +0600

    nav added

---
+++ components/footer.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white border-t">
+      <div className="mx-auto py-10">
+        <div className="text-center text-xs text-black">
+          &copy; Copyright 2024. All rights reserved.
+        </div>
+      </div>
+    </footer>
+  );
+}
