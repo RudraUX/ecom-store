75fc712bd39040f26c0c7e3378d77f0806ac1b49
RudraUX <pollabrock@gmail.com>
2024-01-28 19:05:30 +0600

    billboard added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,15 +1,22 @@
-import React from "react";
-
 import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
 
-function HomePage() {
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("7ce97e47-0f7f-47b7-8a66-d4bc7d21a395");
   return (
     <div>
       <Container>
-        <div className=" space-y-10 pb-10"></div>
+        <div className=" space-y-10 pb-10">
+          <Billboard data={billboard} />
+        </div>
       </Container>
     </div>
   );
-}
+};
 
 export default HomePage;
