057090f9737e671b72dc1fc8fad249f4483d8d7e
RudraUX <pollabrock@gmail.com>
2024-01-29 14:22:39 +0600

    product added

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -2,6 +2,7 @@ import Container from "@/components/ui/container";
 import Billboard from "@/components/billboard";
 import getBillboard from "@/actions/get-billboard";
 import getProducts from "@/actions/get-products";
+import ProductList from "@/components/product-list";
 
 export const revalidate = 0;
 
@@ -13,6 +14,9 @@ const HomePage = async () => {
       <Container>
         <div className=" space-y-10 pb-10">
           <Billboard data={billboard} />
+        </div>
+        <div className=" flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
         </div>
       </Container>
     </div>
---
+++ components/ui/no-results.tsx
@@ -0,0 +1,9 @@
+const NoResults = () => {
+  return (
+    <div className=" flex items-center h-full w-full test-neutral-500">
+      No Results Found
+    </div>
+  );
+};
+
+export default NoResults;
