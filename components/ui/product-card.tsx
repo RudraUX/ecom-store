5c03cf5e3356fabe0e30b69ad1846e45c52df5ec
RudraUX <pollabrock@gmail.com>
2024-02-01 18:56:54 +0600

    ui bug fix

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,18 +1,29 @@
 "use client";
 
 import Image from "next/image";
-import { Expand } from "lucide-react";
+import { Expand, ShoppingCart } from "lucide-react";
 
 import { Product } from "@/types";
 import IconButton from "@/components/ui/icon-button";
+import Currency from "@/components/ui/currency";
+import { useRouter } from "next/navigation";
 
 interface ProductCard {
   data: Product;
 }
 
 const ProductCard: React.FC<ProductCard> = ({ data }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.push(`product/${data?.id}`);
+  };
+
   return (
-    <div className=" bg-white group cursor-pointer rounded-xl border p-3 space-y-4">
+    <div
+      onClick={handleClick}
+      className=" bg-white group cursor-pointer rounded-xl border p-3 space-y-4"
+    >
       <div className=" aspect-square rounded-xl bg-gray-100 relative">
         <Image
           fill
@@ -26,8 +37,19 @@ const ProductCard: React.FC<ProductCard> = ({ data }) => {
               onClick={() => {}}
               icon={<Expand size={20} className="text-gray-600" />}
             />
+            <IconButton
+              onClick={() => {}}
+              icon={<ShoppingCart size={20} className="text-gray-600" />}
+            />
           </div>
         </div>
+      </div>
+      <div>
+        <p className="font-semibold text-lg">{data.name}</p>
+        <p className="text-sm text-gray-500">{data.category?.name}</p>
+      </div>
+      <div>
+        <Currency value={data?.price} />
       </div>
     </div>
   );
